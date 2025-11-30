49ec7d46662e533a8ea693dd9f67a17846641786
ahmed <ahmedyousrysallam@gmail.com>
2025-11-30 17:36:21 +0200

    Update Login Page, ProductsPage

--- src/main/java/Swag/LoginData.java
+++ src/main/java/Swag/LoginData.java
@@ -3,5 +3,5 @@ package Swag;
 public class LoginData {
     public static String []userName = {"standard_user","locked_out_user","problem_user","performance_glitch_user","error_user","visual_user"};
     public static String password = "secret_sauce";
-// Ahmed
+
 }
