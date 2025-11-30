97e2adda8ab961498d489c522109fb16fca4ef5a
AhmedYousrySallam <106488566+AhmedYousrySallam@users.noreply.github.com>
2025-12-01 00:09:52 +0200

    Delete src directory

--- src/main/java/Swag/LoginData.java
+++
@@ -1,7 +0,0 @@
-package Swag;
-
-public class LoginData {
-    public static String []userName = {"standard_user","locked_out_user","problem_user","performance_glitch_user","error_user","visual_user"};
-    public static String password = "secret_sauce";
-
-}
